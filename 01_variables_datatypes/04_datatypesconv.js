75af36aa81db3308590aefb9b8da9abe6f7e2917
mfsisoumayg <soumayg@mindfiresolutions.com>
2024-03-15 21:23:56 +0530

    string to number using parseInt

---
+++ 01_variables_datatypes/04_datatypesconv.js
@@ -0,0 +1,17 @@
+/*
+ string -> number; object; array
+*/
+
+let st1 = "abc"
+let st2 = "1"
+let st3 = "m1"
+let st4 = "11m1"
+// number
+let str1_1 = parseInt(st1)
+let str2_1 = parseInt(st2)
+let str3_1 = parseInt(st3) // get the index til the each item as ASCII code -> get the number (the number should start from 0th index)
+let str4_1 = parseInt(st4) // get the index til the each item as ASCII code -> get the number (the number should start from 0th index)
+console.log(str1_1, typeof str1_1)
+console.log(str2_1, typeof str2_1)
+console.log(str3_1, typeof str3_1)
+console.log(str4_1, typeof str4_1)
