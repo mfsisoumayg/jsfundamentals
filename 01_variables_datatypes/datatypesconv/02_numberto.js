767a853502ba2f7cd7f44a873642b55f7be9f7ba
mfsisoumayg <soumayg@mindfiresolutions.com>
2024-03-19 14:57:43 +0530

    number to bool

--- 01_variables_datatypes/datatypesconv/02_numberto.js
+++ 01_variables_datatypes/datatypesconv/02_numberto.js
@@ -57,10 +57,38 @@ num3_1 = new String(num3)
 num4_1 = new String(num4)
 num5_1 = new String(num5)
 num6_1 = new String(num6)
-console.log(num1, num1_1, typeof num1_1) // 1 [String: '1'] object
-console.log(num2, num2_1, typeof num2_1) // undefined [String: 'undefined'] object
-console.log(num3, num3_1, typeof num3_1) // 2n [String: '2'] object
-console.log(num4, num4_1, typeof num4_1) // 3.14 [String: '3.14'] object
-console.log(num5, num5_1, typeof num5_1) // [Number: 0] [String: '0'] object
-console.log(num6, num6_1, typeof num6_1) // [Number: 3] [String: '3'] object
+// console.log(num1, num1_1, typeof num1_1) // 1 [String: '1'] object
+// console.log(num2, num2_1, typeof num2_1) // undefined [String: 'undefined'] object
+// console.log(num3, num3_1, typeof num3_1) // 2n [String: '2'] object
+// console.log(num4, num4_1, typeof num4_1) // 3.14 [String: '3.14'] object
+// console.log(num5, num5_1, typeof num5_1) // [Number: 0] [String: '0'] object
+// console.log(num6, num6_1, typeof num6_1) // [Number: 3] [String: '3'] object
 
+
+// boolean using Boolean()
+let num1_2 = Boolean(num1)
+let num2_2 = Boolean(num2)
+let num3_2 = Boolean(num3)
+let num4_2 = Boolean(num4)
+let num5_2 = Boolean(num5)
+let num6_2 = Boolean(num6)
+// console.log(num1, num1_2, typeof num1_2) // 1 true boolean
+// console.log(num2, num2_2, typeof num2_2) // undefined false boolean
+// console.log(num3, num3_2, typeof num3_2) // 2n true boolean
+// console.log(num4, num4_2, typeof num4_2) // 3.14 true boolean
+// console.log(num5, num5_2, typeof num5_2) // [Number: 0] true boolean
+// console.log(num6, num6_2, typeof num6_2) // [Number: 3] true boolean
+
+// boolean using new Boolean()
+num1_2 = new Boolean(num1)
+num2_2 = new Boolean(num2)
+num3_2 = new Boolean(num3)
+num4_2 = new Boolean(num4)
+num5_2 = new Boolean(num5)
+num6_2 = new Boolean(num6)
+console.log(num1, num1_2, typeof num1_2) // 1 [Boolean: true] object
+console.log(num2, num2_2, typeof num2_2) // undefined [Boolean: false] object
+console.log(num3, num3_2, typeof num3_2) // 2n [Boolean: true] object
+console.log(num4, num4_2, typeof num4_2) // 3.14 [Boolean: true] object
+console.log(num5, num5_2, typeof num5_2) // [Number: 0] [Boolean: true] object
+console.log(num6, num6_2, typeof num6_2) // [Number: 3] [Boolean: true] object
