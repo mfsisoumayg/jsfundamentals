a951afd90e940b2be8f9b2abfd38c84ce5ce835d
mfsisoumayg <soumayg@mindfiresolutions.com>
2024-03-19 16:25:51 +0530

    operators

---
+++ 01_variables_datatypes/operators/02_arthimetic.js
@@ -0,0 +1,12 @@
+// console.log(2+3)
+// console.log(2-3)
+// console.log(2*3)
+// console.log(2/3)
+// console.log(2%3)
+// console.log(2**3)
+
+let num1 = 1
+console.log(num1, ++num1)
+console.log(num1, num1++)
+console.log(num1, --num1)
+console.log(num1, num1--)
