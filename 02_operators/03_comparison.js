536a28433e530cb6c69bb11cc9ab50c1b37d9e74
mfsisoumayg <161322593+mfsisoumayg@users.noreply.github.com>
2024-03-19 16:55:51 +0530

    move file to operator folder and comparison opt

---
+++ 02_operators/03_comparison.js
@@ -0,0 +1,10 @@
+console.log(1 > 2) // false
+console.log(1 >= 2) // false
+console.log(1 < 2) // true
+console.log(1 <= 2) // true
+console.log(1 != 2) // true
+console.log(1 == 2) // false
+console.log("1" == 1) // true
+console.log("1" === 1) // false
+console.log("1" !== 1) // true
+console.log(typeof BigInt(1n) === "bigint") // true
