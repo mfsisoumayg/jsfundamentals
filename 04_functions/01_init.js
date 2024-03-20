88a23f18773d7565b1578c67d61fe19b6b34cd3c
mfsisoumayg <161322593+mfsisoumayg@users.noreply.github.com>
2024-03-20 15:19:13 +0530

    functions and arrays

--- 04_functions/01_init.js
+++ 04_functions/01_init.js
@@ -16,3 +16,14 @@ console.log(fun3(1,2))
 
 // Parameters-> when declaring a function
 // Arguments-> when calling a function
+
+
+var sum2 = function() {
+    var a = 10, b = 20
+    sum = a + b
+    console.log(`sum: ${sum}`)
+    return sum
+}
+
+console.log(typeof sum2)
+console.log(`sum2: ${sum2}`)
---
+++ 05_arrays/01_init.js
@@ -0,0 +1,11 @@
+let arr1 = []
+let arr2 = Array()
+let arr3 = Array(3)
+let arr4 = new Array()
+let arr5 = new Array(3)
+
+console.log(arr1, typeof arr1)
+console.log(arr2, typeof arr2)
+console.log(arr3, typeof arr3)
+console.log(arr4, typeof arr4)
+console.log(arr5, typeof arr5)
