5d80701f150a5b3dcca6e6630e6f8ce963a94410
mfsisoumayg <161322593+mfsisoumayg@users.noreply.github.com>
2024-03-20 19:24:50 +0530

    objects init

---
+++ 07_objects/01_init.js
@@ -0,0 +1,5 @@
+let obj1 = Object()
+let obj2 = Object(1)
+
+console.log(obj1, typeof obj1) // {} object
+console.log(obj2, typeof obj2) // [Number: 1] object
---
+++ 07_objects/02_methods.js
@@ -0,0 +1,8 @@
+function Demo(name) {
+    this.name = name
+}
+console.log(typeof Demo)
+
+
+const demo1 = new Demo("JS")
+console.log(demo1.name)
