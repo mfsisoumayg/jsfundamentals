684af8e0cb07ca3e90a353ea8cc07c61ca5ba730
mfsisoumayg <161322593+mfsisoumayg@users.noreply.github.com>
2024-03-20 17:27:49 +0530

    arrays

---
+++ 05_arrays/02_methods.js
@@ -0,0 +1,26 @@
+let arr1 = []
+
+
+// pop, push, shift, unshift
+arr1.push(3)
+console.log(arr1)
+arr1.push(1)
+console.log(arr1)
+arr1.push(4)
+console.log(arr1)
+arr1.pop()
+console.log(arr1)
+arr1.unshift(6)
+console.log(arr1)
+arr1.unshift(7)
+console.log(arr1)
+arr1.unshift(9)
+console.log(arr1)
+arr1.unshift(10)
+console.log(arr1)
+arr1.shift()
+console.log(arr1)
+
+// sort
+arr1.sort()
+console.log(arr1)
